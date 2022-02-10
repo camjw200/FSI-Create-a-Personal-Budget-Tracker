308fce0364d6f4b86428e37ffb46d2197b789736
camjw200 <cameronjwilliams10@gmail.com>
2022-02-09 19:02:20 -0600

    bonus content

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ let monthlyExpenses = 0;
 let annualExpenses = 0;
 
 // Your Code Here
-for (i = 0; i < weeklyExpenseQuestions.length; i++) {
+//for (i = 0; i < weeklyExpenseQuestions.length; i++) {
+//  let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
+//  weeklyExpenses = weeklyExpenses + answer;
+//}
+//for (i = 0; i < monthlyExpenseQuestions.length; i++) {
+//  let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+//  monthlyExpenses = monthlyExpenses + answer;
+//}
+//for (i = 0; i < annualExpenseQuestions.length; i++) {
+//  let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+//  annualExpenses = annualExpenses + answer;
+//}
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+while (i < weeklyExpenseQuestions.length) {
   let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]));
   weeklyExpenses = weeklyExpenses + answer;
+  i++;
 }
-for (i = 0; i < monthlyExpenseQuestions.length; i++) {
-  let answer = parseFloat(window.prompt(monthlyExpenseQuestions[i]));
+while (j < monthlyExpenseQuestions.length) {
+  let answer = parseFloat(window.prompt(monthlyExpenseQuestions[j]));
   monthlyExpenses = monthlyExpenses + answer;
+  j++;
 }
-for (i = 0; i < annualExpenseQuestions.length; i++) {
-  let answer = parseFloat(window.prompt(annualExpenseQuestions[i]));
+while (k < annualExpenseQuestions.length) {
+  let answer = parseFloat(window.prompt(annualExpenseQuestions[k]));
   annualExpenses = annualExpenses + answer;
+  k++;
 }
